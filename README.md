98dd60a51d34673b61f93a2f084ba7bb8fe1827b
rxhl <maniaxecorp@gmail.com>
2020-02-13 22:26:23 -0500

    Update account form

--- README.md
+++ README.md
@@ -5,9 +5,10 @@ Flock is a flask app including:
 
 1. Persistent store (sqlite)
 2. Session management (Flask-Login)
-3. Basic UI uisng jinja (feels ancient after writing `JSX` for so long)
-4. Hosting (Linode)
+3. Basic UI using jinja (feels ancient after writing `JSX` for so long)
+4. Image upload and editing (Pillow)
 5. Well defined project structure (venv, packages etc)
+6. Hosting (Linode)
 
 ### Development
 
--- flock/routes.py
+++ flock/routes.py
@@ -1,10 +1,13 @@
+import os
+import secrets
+from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
 from flask_login import login_user, current_user, logout_user, login_required
 
 # Direct import from __init__.py file
 from flock import app, db, bcrypt
 
-from flock.forms import RegistrationForm, LoginForm
+from flock.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from flock.models import User, Post
 
 posts = [
@@ -79,7 +82,43 @@ def logout():
     return redirect(url_for("login"))
 
 
-@app.route("/account")
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+
+    # Using _ as placeholder return value as we are not gonna use it anywhere.
+    _, f_ext = os.path.splitext(form_picture.filename)
+
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, "static/profile_pics", picture_fn)
+
+    # Resize large images
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+
+    return picture_fn
+
+
+@app.route("/account", methods=["GET", "POST"])
 @login_required
 def account():
-    return render_template("account.html", title="Account")
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash("Your account has been updated!", "success")
+        return redirect(url_for("account"))
+    elif request.method == "GET":
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for("static", filename="profile_pics/" + current_user.image_file)
+    return render_template(
+        "account.html", title="Account", image_file=image_file, form=form
+    )
+
