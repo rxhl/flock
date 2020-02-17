635c8591000d3c4b0d220140599cf0c10af04fad
rxhl <maniaxecorp@gmail.com>
2020-02-16 21:52:13 -0500

    Update env variables

--- flock/__init__.py
+++ flock/__init__.py
@@ -17,6 +17,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "892627b4f69774cf4859685cdea0e6b5"
@@ -29,8 +32,8 @@ login_manager.login_message_category = "info"
 app.config["MAIL_SERVER"] = "smtp.googlemail.com"
 app.config["MAIL_PORT"] = 587
 app.config["MAIL_USE_TLS"] = True
-app.config["MAIL_USERNAME"] = os.environ.get("EMAIL_USER")
-app.config["MAIL_PASSWORD"] = os.environ.get("EMAIL_PASS")
+app.config["MAIL_USERNAME"] = os.getenv("EMAIL_USER")
+app.config["MAIL_PASSWORD"] = os.getenv("EMAIL_PASS")
 mail = Mail(app)
 
 """
