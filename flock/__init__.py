1b8a238035cddb19bd10ad5cdcf0fcbc30637865
rxhl <maniaxecorp@gmail.com>
2020-02-16 21:59:13 -0500

    Add more env

--- flock/__init__.py
+++ flock/__init__.py
@@ -22,15 +22,15 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "892627b4f69774cf4859685cdea0e6b5"
+app.config["SECRET_KEY"] = os.getenv("SECRET")
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = "login"
 login_manager.login_message_category = "info"
-app.config["MAIL_SERVER"] = "smtp.googlemail.com"
-app.config["MAIL_PORT"] = 587
+app.config["MAIL_SERVER"] = os.getenv("EMAIL_SERVER")
+app.config["MAIL_PORT"] = os.getenv("EMAIL_PORT")
 app.config["MAIL_USE_TLS"] = True
 app.config["MAIL_USERNAME"] = os.getenv("EMAIL_USER")
 app.config["MAIL_PASSWORD"] = os.getenv("EMAIL_PASS")
